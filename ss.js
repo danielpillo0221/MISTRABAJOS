d96e15f223198b6ec6821d510bfcac46fbafe8c8
danilo <carmonadaniel0222@gmail.com>
2023-12-06 20:06:06 -0500

    AA

--- ss.js
+++ ss.js
@@ -1,6 +1,14 @@
 
 function agregarPedidos (){
-    const pedido=document.getElementById("Pedidos").value;
-    
-    
+    const producto=document.getElementById("Pedidos").value;
+    const cantidad=document.getElementById("Cantidad").value;
+   
+    let pedidos =JSON.parse(localStorage.getItem("pedidos"))|| [] ;
+
+     pedidos.push({producto, cantidad})
+    console.log(pedidos)
+
+
+      localStorage.setItem("pedidos", JSON.stringify(pedidos));
+      document.getElementById("formulario").reset;
 }
